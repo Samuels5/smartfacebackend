9c0c5506a08adc4eb2cd34eb99339f06499561bb
Samuels5 <samimule20@gmail.com>
2024-11-05 19:39:35 +0300

    cors cors

--- server.js
+++ server.js
@@ -107,18 +107,7 @@ const app = express();
 const PORT = 5000;
 
 // Enable CORS for all routes with specific headers for preflight requests
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // Allow specific HTTP methods
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(cors());
 
 // Middleware to parse JSON bodies
 app.use(express.json());
